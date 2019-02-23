15123e8706d3482663f74e88b25b10c131542b34
benzach0421 <benzach0421@gmail.com>
2019-02-22 23:07:29 -0700

    Added another "<br>" tag to the date alert message

--- date.php
+++ date.php
@@ -4,6 +4,6 @@ session_start();
 
 $fp = fopen("log.html", 'a');
 
-fwrite($fp, "<div class='msgln' style='text-align:center;'><i>- - - - - ".date("l, jS \of F Y")." - - - - -</i><br></div>");
+fwrite($fp, "<div class='msgln' style='text-align:center;'><i>- - - - - ".date("l, jS \of F Y")." - - - - -</i><br><br></div>");
 fclose($fp);
 ?>
